7abe4dc15bfdfcf3aa37ba576d63f3ef6c3a21be
HASHEBUR RAHMAN <you@example.com>
2023-09-15 11:11:08 +0600

    typography

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Typography } from '@mui/material';
+import './App.css';
+import { MuiTypography } from './Components/MuiTypography';
+
+function App() {
+  return (
+    <div className="App">
+      <MuiTypography></MuiTypography>
+      <Typography variant="h1">h1. Heading</Typography>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MuiTypography.tsx
@@ -0,0 +1,29 @@
+import { Typography } from '@mui/material';
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            MuiTypography
+
+            <Typography variant="h1">h1. Heading</Typography>
+            <Typography variant="h2">h2. Heading</Typography>
+            <Typography variant="h3">h3. Heading</Typography>
+            <Typography variant="h4">h4. Heading</Typography>
+            <Typography variant="h5">h5. Heading</Typography>
+            <Typography variant="h6">h6. Heading</Typography>
+
+            <Typography variant="subtitle1">subtitle-1</Typography>
+            <Typography variant="subtitle2">subtitle-2</Typography>
+
+
+            <Typography> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo facere repellat voluptates porro, enim consequatur similique est omnis corrupti accusamus culpa alias dolorem ullam distinctio in dolor deleniti animi? Nulla ab officia, alias, repellat praesentium unde sapiente adipisci, incidunt ullam maxime architecto nostrum fugit earum vitae ratione eveniet laborum consectetur!</Typography>
+            <Typography variant="body2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dolores fugiat deleniti! Impedit, fugit adipisci! Asperiores veniam ad in, illo eius voluptatum dolorem nobis fugiat perferendis error enim nulla libero! Vero inventore eum magni voluptate officiis voluptas sapiente earum odio ipsam similique aliquid minus nam dolor alias animi esse doloribus cumque quas quaerat obcaecati, id aperiam suscipit facere. Excepturi eaque aliquam amet, nisi quisquam magnam nobis nihil non blanditiis sapiente, veritatis animi? Animi earum praesentium ad nisi. Nihil ex aperiam labore earum explicabo quibusdam, voluptatum ipsum repellendus soluta quas cum fuga deserunt sit consequatur laborum rerum pariatur ullam eum distinctio.</Typography>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nesciunt deleniti quod officiis ipsum eius laudantium minus, mollitia natus alias nisi maiores nulla quibusdam reiciendis expedita aspernatur provident, vero minima.</p>
+
+
+
+
+        </div>
+    )
+}
